edeba3443465946134bc51c8bb697520c122c464
Premeeii <peam972547@gmail.com>
2026-08-12 03:18:09 +0700

    chore: add .gitignore file and application-example.properties

---
+++ backend/lafam/src/main/java/premeees/lafam/LafamApplication.java
@@ -0,0 +1,13 @@
+package premeees.lafam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LafamApplication.class, args);
+	}
+
+}
---
+++ backend/lafam/src/test/java/premeees/lafam/LafamApplicationTests.java
@@ -0,0 +1,13 @@
+package premeees.lafam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
