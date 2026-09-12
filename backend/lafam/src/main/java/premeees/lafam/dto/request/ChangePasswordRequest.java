fe8021b3f090d07e46bb44fc640e0db21330d04e
Premeeii <peam972547@gmail.com>
2026-09-13 00:51:52 +0700

    feat: create request for change password

---
+++ backend/lafam/src/main/java/premeees/lafam/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,32 @@
+package premeees.lafam.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+    private String newPassword;
+
+    public ChangePasswordRequest() {}
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
