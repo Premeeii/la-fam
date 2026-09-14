45ddfd30406100ff80053a9d9ccd6d103100d28e
Premeeii <peam972547@gmail.com>
2026-09-14 17:22:59 +0700

    refactor: delte unusable and set fetch plan

--- backend/lafam/src/main/java/premeees/lafam/Repository/GroupMemberRepository.java
+++ backend/lafam/src/main/java/premeees/lafam/Repository/GroupMemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import premeees.lafam.Entity.GroupMember;
@@ -11,10 +12,13 @@ import premeees.lafam.Entity.GroupMember;
 public interface GroupMemberRepository extends JpaRepository<GroupMember, UUID> {
     List<GroupMember> findByUserId(UUID userId);
     List<GroupMember> findAllByUserId(UUID userId);
+    
+    @EntityGraph(attributePaths = {"group", "user"})
     List<GroupMember> findAllByUserIdAndGroupDeletedAtIsNull(UUID userId);
 
     List<GroupMember> findAllByUserIdAndGroupDeletedAtIsNullAndLeavedAtIsNull(UUID userId);
 
+    @EntityGraph(attributePaths = { "group", "user" })
     List<GroupMember> findAllByGroupIdAndLeavedAtIsNull(UUID groupId);
 
     Optional<GroupMember> findByGroupIdAndUserId(UUID groupId, UUID userId);
