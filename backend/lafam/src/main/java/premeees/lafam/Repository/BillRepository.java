2ffbe273598cc440a00cc320d852d5ec587d3dca
Premeeii <peam972547@gmail.com>
2026-09-23 01:03:04 +0700

    feat: add repository on event and bill

--- backend/lafam/src/main/java/premeees/lafam/Repository/BillRepository.java
+++ backend/lafam/src/main/java/premeees/lafam/Repository/BillRepository.java
@@ -3,6 +3,7 @@ package premeees.lafam.Repository;
 import java.util.List;
 import java.util.UUID;
 
+import premeees.lafam.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import premeees.lafam.Entity.Bill;
@@ -11,4 +12,5 @@ public interface BillRepository extends JpaRepository<Bill, UUID> {
     List<Bill> findAllByGroupId(UUID groupId);
     List<Bill> findAllByGroupIdAndCreatedById(UUID groupId, UUID createdById);
     List<Bill> findAllByGroupIdAndBillCategoryId(UUID groupId, UUID categoryId);
+    void deleteAllByCreatedBy(User user);
 }
